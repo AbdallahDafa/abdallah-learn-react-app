d1db6774b3fbd5fe4f038f1784d7ab9f9f5bfe04
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-06-25 15:42:43 +0300

    learn.html file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+ 
 import './App.css';
+ 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  function App(){
+    return (
+      <div>
+        <h1>Welcome to App.js File</h1>
+        <button onClick={redirectToLearn}>Go to Learn Page</button>
+      </div>
+    );
+  }
+
+  const redirectToLearn = () => {
+    window.location.href = './learn.html'; // or full path if hosted elsewhere
+  };
+
+   
 
 export default App;
